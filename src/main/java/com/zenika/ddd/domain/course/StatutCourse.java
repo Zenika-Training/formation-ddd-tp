802252063eb15c16b369df484227629184209109
Gweltaz Niquel <gweltaz.niquel@zenika.com>
2023-11-23 11:19:17 +0100

    feat: init model

---
+++ src/main/java/com/zenika/ddd/domain/course/StatutCourse.java
@@ -0,0 +1,9 @@
+package com.zenika.ddd.domain.course;
+
+public enum StatutCourse {
+    ACCEPTED,
+    TO_COLLECT,
+    COLLECTED,
+    SHIPPING,
+    CANCELLED
+}
---
+++ src/main/java/com/zenika/ddd/domain/shared/Position.java
@@ -0,0 +1,4 @@
+package com.zenika.ddd.domain.shared;
+
+public record Position(double latitude, double longitude) {
+}
