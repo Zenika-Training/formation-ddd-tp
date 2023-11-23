0e375e002c5208ea4779b870b314229174ce96f5
Gweltaz Niquel <gweltaz.niquel@zenika.com>
2023-11-23 09:22:35 +0100

    feat: init

---
+++ src/main/java/com/zenika/ddd/DddApplication.java
@@ -0,0 +1,16 @@
+package com.zenika.ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class DddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddApplication.class, args);
+    }
+
+}
