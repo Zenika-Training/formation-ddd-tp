2041e5eb5b6107d23d0697e4c58b49a15c056f78
Alexandre Fruchaud <alexandre.fruchaud@gmail.com>
2024-03-05 15:43:06 +0100

    reinit

--- src/main/java/com/brett/catalog/domain/PricePerDay.java
+++
@@ -1,7 +0,0 @@
-package com.brett.catalog.domain;
-
-public record PricePerDay(AmountInCents amount) {
-    public static PricePerDay of(AmountInCents amount) {
-        return new PricePerDay(amount);
-    }
-}
